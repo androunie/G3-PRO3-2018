fe42e9899bd8f7ef009b70ac4dd9ae1c69a2a955
Androunie <andrea.perri.chretien@gmail.com>
2018-11-28 16:49:44 +0100

    ajout d'action avec les 2 btns

--- app/src/main/java/com/example/andreaperri/g3_pro3_2018/MainActivity.java
+++ app/src/main/java/com/example/andreaperri/g3_pro3_2018/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.andreaperri.g3_pro3_2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import 	android.widget.Button;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_trombinoscope;
+    Button btn_addPerson;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_trombinoscope = (Button) findViewById(R.id.btn_trombinoscope);
+
+        btn_trombinoscope.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Bienvenue au Trombinoscope", LENGTH_LONG).show();
+            }
+        });
+
+
+        btn_addPerson = (Button) findViewById(R.id.btn_addpersonredirect);
+        btn_addPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Bienvenue au Trombinoscope", LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
